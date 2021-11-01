7f05ad2216aa5df1a6af5fb90fa8850102646483
lososi <78958316+lososi@users.noreply.github.com>
2021-11-01 17:42:10 +0000

    Update larbs.sh

--- larbs.sh
+++ larbs.sh
@@ -14,7 +14,7 @@ while getopts ":a:r:b:p:h" o; do case "${o}" in
 	*) printf "Invalid option: -%s\\n" "$OPTARG" && exit 1 ;;
 esac done
 
-[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lukesmithxyz/voidrice.git"
+[ -z "$dotfilesrepo" ] && dotfilesrepo="https://github.com/lososi/voidrice.git"
 [ -z "$progsfile" ] && progsfile="https://raw.githubusercontent.com/lososi/LARBS/master/progs.csv"
 [ -z "$aurhelper" ] && aurhelper="paru"
 [ -z "$repobranch" ] && repobranch="master"
